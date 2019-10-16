f02a1dcf45bd71720ee498d81a34e92b0ccde478
Artem <dsafsfsgfjnfghsdf@users.noreply.github.com>
2019-10-16 15:39:27 +0300

    Project was created. Task 6 was completed

---
+++ Module3_6/Module3_6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введте количество элементов массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Random rand = new Random();
+
+            Console.WriteLine("Исходный массив:");
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = rand.Next(-5, 5);
+                Console.Write("{0, 3}", $"{arr[i]}");
+            }
+
+            Console.WriteLine("\nПреобразованный массив:");
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] *= -1;
+                Console.Write("{0, 3}", $"{arr[i]}");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
