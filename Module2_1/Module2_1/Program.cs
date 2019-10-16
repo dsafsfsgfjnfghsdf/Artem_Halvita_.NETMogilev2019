90c012842b170e37b67cc4c01ab372959692b138
Artem <dsafsfsgfjnfghsdf@users.noreply.github.com>
2019-10-16 22:12:49 +0300

    Изменил названия переменных согласно код конвенции

--- Module2_1/Module2_1/Program.cs
+++ Module2_1/Module2_1/Program.cs
@@ -11,14 +11,14 @@ namespace Module2_1
         static void Main(string[] args)
         {
             Console.Write("Введите N (число компаний, работающих в стране): ");
-            int N = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Введите M (налог, который взымает государство): ");
-            int M = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
 
-            int CompanyProfits = 500;
+            int companyProfits = 500;
 
-            Console.WriteLine("Суммарный налог, который получит государство: " + CompanyProfits * N / 100 * M);
+            Console.WriteLine("Суммарный налог, который получит государство: " + companyProfits * n / 100 * m);
         }
     }
 }
