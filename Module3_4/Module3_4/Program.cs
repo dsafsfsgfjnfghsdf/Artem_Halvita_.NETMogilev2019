ce9a77da18010b1c4aec7fb22070b1da0f0c9ad5
Artem <dsafsfsgfjnfghsdf@users.noreply.github.com>
2019-10-16 15:29:41 +0300

    Project was created. Task 4 was completed

---
+++ Module3_4/Module3_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число для преобразования: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int inverted = 0;
+
+            while (number != 0)
+            {
+                inverted = inverted * 10 + number % 10;
+                number /= 10;
+            }
+
+            Console.WriteLine($"Перевернутое число: {inverted}");
+        }
+    }
+}
