a151af3b3fa10c9f5aef20225a1ac6a2410efcd2
Artem <dsafsfsgfjnfghsdf@users.noreply.github.com>
2019-10-16 23:47:01 +0300

    Update Program.cs

--- Module3_1/Module3_1/Program.cs
+++ Module3_1/Module3_1/Program.cs
@@ -10,47 +10,47 @@ namespace Module3_1
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите множитель: ");
-            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите первый множитель: ");
+            int firstMultiplier = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Введите второй множитель: ");
-            int b = Convert.ToInt32(Console.ReadLine());
+            int secondMutiplier = Convert.ToInt32(Console.ReadLine());
 
-            if (a >= 0 && b >= 0)
+            if (firstMultiplier >= 0 && secondMutiplier >= 0)
             {
                 int multiply = 0;
 
-                for (int i = 0; i < b; i++)
-                    multiply += a;
+                for (int i = 0; i < secondMutiplier; i++)
+                    multiply += firstMultiplier;
 
-                Console.WriteLine($"{a} * {b} = {multiply}");
+                Console.WriteLine($"{firstMultiplier} * {secondMutiplier} = {multiply}");
             }
-            else if (a <= 0 && b >= 0)
+            else if (firstMultiplier <= 0 && secondMutiplier >= 0)
             {
-                int multiply = ~a + 1 + a;
+                int multiply = ~firstMultiplier + 1 + firstMultiplier;
 
-                for (int i = 0; i < b; i++)
-                    multiply += a;
+                for (int i = 0; i < secondMutiplier; i++)
+                    multiply += firstMultiplier;
 
-                Console.WriteLine($"{a} * {b} = {multiply}");
+                Console.WriteLine($"{firstMultiplier} * {secondMutiplier} = {multiply}");
             }
-            else if (a >= 0 && b <= 0)
+            else if (firstMultiplier >= 0 && secondMutiplier <= 0)
             {
-                int multiply = ~b + 1 + b;
+                int multiply = ~secondMutiplier + 1 + secondMutiplier;
 
-                for (int i = 0; i < a; i++)
-                    multiply += b;
+                for (int i = 0; i < firstMultiplier; i++)
+                    multiply += secondMutiplier;
 
-                Console.WriteLine($"{a} * {b} = {multiply}");
+                Console.WriteLine($"{firstMultiplier} * {secondMutiplier} = {multiply}");
             }
-            else if (a <= 0 && b <= 0)
+            else if (firstMultiplier <= 0 && secondMutiplier <= 0)
             {
-                int multiply = ~a + 1 + a;
+                int multiply = ~firstMultiplier + 1 + firstMultiplier;
 
-                for (int i = 0; i < (~b + 1); i++)
-                    multiply += a;
+                for (int i = 0; i < (~secondMutiplier + 1); i++)
+                    multiply += firstMultiplier;
 
-                Console.WriteLine($"{a} * {b} = {-multiply}");
+                Console.WriteLine($"{firstMultiplier} * {secondMutiplier} = {-multiply}");
             }
         }
     }
