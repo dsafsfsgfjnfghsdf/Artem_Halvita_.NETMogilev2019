6ae74ec83229f0540429a5841cbb99c61effacd8
Artem <dsafsfsgfjnfghsdf@users.noreply.github.com>
2019-10-16 15:24:41 +0300

    Project was created. Task 2 was completed

---
+++ Module3_2/Module3_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество натуральных чисел: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int i = 0;
+            while (n != 0)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write($"{i} ");
+                    n--;
+                }
+                i++;
+            }
+        }
+    }
+}
