9e25cb70ec073df901adc94e91d6b5cf3f24aa45
Artem <dsafsfsgfjnfghsdf@users.noreply.github.com>
2019-10-16 14:41:40 +0300

    Project was created. Task 2 was completed

---
+++ Module2_2/Module2_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if ((N % 2 == 0) && (N >= 18))
+                Console.WriteLine("Поздравляю с 18-и летием");
+
+            else if ((N % 2 != 0) && (N > 13) && (N < 18))
+                Console.WriteLine("Поздравляю с переходом в старшую школу");
+        }
+    }
+}
